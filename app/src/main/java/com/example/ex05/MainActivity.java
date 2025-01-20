11ddce4f5ed3be66f9bf37a317d306d01af7f050
hanna <ht0561@bs.amlnet.k12.il>
2025-01-20 13:43:47 +0200

    task1

--- app/src/main/java/com/example/ex05/MainActivity.java
+++ app/src/main/java/com/example/ex05/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.ex05;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,14 +13,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    
+    Button btn;
+    TextView textV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn.findViewById(R.id.btn);
+        textV.findViewById(R.id.textV);
 
     }
 
     public void go(View view) {
+        textV.setTextColor(Color.RED);
+        textV.setText("Oh, yea, I've been clicked!");
     }
 }
